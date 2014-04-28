66556a458092d9d4f247792b8eda057d48c6b0ca
Marius Edelsbrunner <marius@softerik.com>
2014-04-28 01:09:31 -0300

    WI 4 : Google Maps Trace.

--- js/app_helpers.js
+++ js/app_helpers.js
@@ -19,4 +19,33 @@ Ember.Handlebars.helper('substringHelper', function(value) {
 
 });
 
+    App.Trace = Ember.View.extend({
+        didInsertElement: function () {
+            var container = $('#googleTrace')[0];
+            $(container).css({ height: '220px', width: '50%' });
+            var encodedTrace = this.get('value');
+            if (encodedTrace == null) 
+				return;
+            var decodedTrace = google.maps.geometry.encoding.decodePath(encodedTrace);
+            var center = new google.maps.LatLng(0, 0);
+
+            var options = {
+                center: center,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var trace = new google.maps.Polyline({
+                path: decodedTrace
+            });
+
+            var map = new google.maps.Map(container, options);
+
+            var bounds = new google.maps.LatLngBounds();
+            trace.getPath().forEach(function (LatLng) {
+                bounds.extend(LatLng);
+            });
+            map.fitBounds(bounds);
+            trace.setMap(map);
+        }
+    });
 })();
